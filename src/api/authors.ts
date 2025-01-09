565ff9144bdd02fe29d0af69d614a664155b355f
yysofiyan <yanyansofiyan89outlook.com>
2025-01-10 00:12:09 +0700

    update API endpoint for fetching authors

--- src/api/authors.ts
+++ src/api/authors.ts
@@ -1,7 +1,7 @@
 import { SintaProfile } from "../types";
 
 export async function fetchAuthors(): Promise<SintaProfile[]> {
-  const response = await fetch('http://localhost:5001/api/authors'); // Ganti dengan URL API Anda
+  const response = await fetch('https://yysofiyan-savvytomatotoad.web.val.run/api/authors'); // URL API Local http://localhost:5001/api/authors
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
